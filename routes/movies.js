f6989d9195816f3fd25b79e0a1e9fa38be48799d
sophiegreen4 <sophgreen4@gmail.com>
2025-05-09 11:47:56 +0100

    get, post, patch and delete movies

--- routes/movies.js
+++ routes/movies.js
@@ -4,23 +4,39 @@ const { Movie, User } = require("../models/index.js");
 const router = express.Router();
 
 router.post("/", async function (req, res) {
-  // TODO: Create a movie.
+  const movie = await Movie.create(req.body);
+  res.status(201).send(movie);
 });
 
+
 router.get("/", async function (req, res) {
-  // TODO: Get all movies.
+  const movies = await Movie.findAll();
+  res.json(movies);
 });
 
 router.get("/:movieId", async function (req, res) {
-  // TODO: Get one movie.
+  const movie = await Movie.findByPk(req.params.movieId);
+  res.json(movie);
 });
 
 router.patch("/:movieId", async function (req, res) {
-  // TODO: Update one movie.
+  const movie = await Movie.findByPk(req.params.movieId);
+    if (!movie){
+      res.status(404).json({ error: "Movie not found"});
+    }else{
+      await movie.update(req.body);
+      res.json(movie);
+    }
 });
 
 router.delete("/:movieId", async function (req, res) {
-  // TODO: Delete one movie.
+  const movie = await Movie.findByPk(req.params.movieId);
+  if (movie){
+    await movie.destroy();
+    res.status(204).send();
+  } else{
+    res.status(404).send();
+  }
 });
 
 router.get("/:movieId/users", async function (req, res) {
