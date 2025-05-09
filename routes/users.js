1934bd27d1b0d4a3b7b4caaffa11970495386579
sophiegreen4 <sophgreen4@gmail.com>
2025-05-09 14:10:36 +0100

    users routers added

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,8 @@ const { Movie, User } = require("../models/index.js");
 const router = express.Router();
 
 router.post("/", async function (req, res) {
-  // TODO: Create a user.
+  const user = await User.create(req.body);
+  res.status(201).send(user);
 });
 
 router.get("/", async function (req, res) {
@@ -15,23 +16,54 @@ router.get("/", async function (req, res) {
 
 router.get("/:userId", async function (req, res) {
   // TODO: Get one user.
+  const user = await User.findByPk(req.params.userId);
+  res.json(user);
 });
 
 router.patch("/:userId", async function (req, res) {
   // TODO: Update one user.
+  const user = await User.findByPk(req.params.userId);
+    if (!user){
+      res.status(404).json({ error: "User not found"});
+    }else{
+      await user.update(req.body);
+      res.json(user);
+    }
 });
 
 router.delete("/:userId", async function (req, res) {
   // TODO: Delete one user.
+  const user = await User.findByPk(req.params.userId);
+  if (user){
+    await user.destroy();
+    res.status(204).send();
+  } else{
+    res.status(404).send();
+  }
 });
 
 router.get("/:userId/movies", async function (req, res) {
   // TODO: Get all movies watched by one user.
+  const user = await User.findByPk(req.params.userId);
+  if (user){
+    const movies = await user.getMovies();
+    res.status(200).send(movies);
+  } else{
+    res.status(404).send();
+  }
 });
 
 router.post("/:userId/movies/:movieId", async function (req, res) {
   // TODO: Associate a user with a movie they have watched.
   // HINT: No body required; all the required info is in the params!
+  const movie = await Movie.findByPk(req.params.movieId);
+  const user = await User.findByPk(req.params.userId)
+  if (!movie || !user) {
+    return res.status(404).send();
+  } else {
+    await user.addMovie(movies);
+    res.status(200).send();
+  }
 });
 
 module.exports = router;
